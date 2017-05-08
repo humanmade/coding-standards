fb04de5f2273a3819b498681d621706e776d3632
Robert O'Rourke <robert@o-rourke.org>
2017-05-08 19:55:32 +0100

    fix tests

--- HM/Tests/Layout/OrderUnitTest.php
+++ HM/Tests/Layout/OrderUnitTest.php
@@ -1,10 +1,6 @@
 <?php
 
-namespace HM\Tests\Layout;
-
-use AbstractSniffUnitTest;
-
-class OrderUnitTest extends AbstractSniffUnitTest {
+class HM_Tests_Layout_OrderUnitTest extends AbstractSniffUnitTest {
 
 	/**
 	 * Returns the lines where errors should occur.
@@ -12,23 +8,18 @@ class OrderUnitTest extends AbstractSniffUnitTest {
 	 * @return array <int line number> => <int number of errors>
 	 */
 	public function getErrorList() {
-		return array(
-			3 => 1,
-			5 => 1,
-			7 => 1,
-			8 => 1,
-			12 => 1,
-			23 => 1,
-			24 => 1,
-		);
+		return [
+			1  => 1,
+		];
 	}
+
 	/**
 	 * Returns the lines where warnings should occur.
 	 *
 	 * @return array <int line number> => <int number of warnings>
 	 */
 	public function getWarningList() {
-		return array();
+		return [];
 	}
 
 }
