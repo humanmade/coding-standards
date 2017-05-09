6c519bc81706a1ae1884cafdfd0df2b3b7458dd5
Robert O'Rourke <robert@o-rourke.org>
2017-05-09 10:45:54 +0100

    fix order sniff, add tests

---
+++ HM/Tests/Layout/OrderUnitTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class HM_Tests_Layout_OrderUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return [
+			1  => 1,
+		];
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return [];
+	}
+
+}
