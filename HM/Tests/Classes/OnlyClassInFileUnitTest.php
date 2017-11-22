0e7910952781f22d1cfa8da418bf307d271d90b8
Ryan McCue <me@ryanmccue.info>
2017-11-22 17:28:26 +1000

    Add tests for all our PHP sniffs

---
+++ HM/Tests/Classes/OnlyClassInFileUnitTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace HM\Tests\Classes;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+class OnlyClassInFileUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return [];
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		$file = func_get_arg( 0 );
+		list( $_, $type, $variant ) = explode( '.', $file, 3 );
+		if ( $type !== 'fail' ) {
+			return [];
+		}
+
+		return [
+			1 => 1,
+		];
+	}
+
+}
---
+++ HM/Tests/Namespaces/NoLeadingSlashOnUseUnitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace HM\Tests\Namespaces;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+class NoLeadingSlashOnUseUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		$file = func_get_arg( 0 );
+		switch ( $file ) {
+			case 'NoLeadingSlashOnUseUnitTest.success':
+				return [];
+
+			case 'NoLeadingSlashOnUseUnitTest.fail':
+				return [
+					5 => 1,
+					6 => 1,
+				];
+		}
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return [];
+	}
+
+}
