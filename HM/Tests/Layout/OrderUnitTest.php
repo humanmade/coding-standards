a5aa7232dfa722bb56105927bcbe74bf027fe2a0
Ryan McCue <me@ryanmccue.info>
2017-11-22 14:22:03 +1000

    Use new classes for testing

--- HM/Tests/Layout/OrderUnitTest.php
+++ HM/Tests/Layout/OrderUnitTest.php
@@ -1,6 +1,10 @@
 <?php
 
-class HM_Tests_Layout_OrderUnitTest extends AbstractSniffUnitTest {
+namespace HM\Tests\Layout;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+class OrderUnitTest extends AbstractSniffUnitTest {
 
 	/**
 	 * Returns the lines where errors should occur.
