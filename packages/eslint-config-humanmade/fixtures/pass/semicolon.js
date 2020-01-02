666d0f7c27c5979bf093b23fe40cf7c28c6df5a2
Matthew Haines-Young <matthewhainesyoung@gmail.com>
2020-01-02 13:38:07 +0000

    Enforce semicolons

---
+++ packages/eslint-config-humanmade/fixtures/pass/semicolon.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+// Missing semicolon.
+const testMissingSemicolon = 'foo';
+
+// Missing semicolon.
+const testSemiSpacing = 'bar';
+
+// Double semicolon.
+const testDoubleSemiColon = 5;
+
+const testFunc = function () {
+	// ...
+};
