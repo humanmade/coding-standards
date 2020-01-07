f57d2c345a419c6a4a011b4ea22eb34af1a8b484
Mike Selander <mikeselander@gmail.com>
2020-01-07 08:04:46 -0700

    Add method docblocks

--- HM/Sniffs/Whitespace/MultipleEmptyLinesSniff.php
+++ HM/Sniffs/Whitespace/MultipleEmptyLinesSniff.php
@@ -16,10 +16,28 @@ use PHP_CodeSniffer\Sniffs\Sniff;
  */
 class MultipleEmptyLinesSniff implements Sniff {
 
+	/**
+	 * Registers the tokens that this sniff wants to listen for.
+	 *
+	 * @return array
+	 * @see    Tokens.php
+	 */
 	public function register() {
 		return [ T_WHITESPACE ];
 	}
 
+	/**
+	 * Called when one of the token types that this sniff is listening for is found.
+	 *
+	 * The stackPtr variable indicates where in the stack the token was found.
+	 * A sniff can acquire information this token, along with all the other
+	 * tokens within the stack by first acquiring the token stack:
+	 *
+	 * @param File $phpcsFile The PHP_CodeSniffer file where the token was found.
+	 * @param int  $stackPtr  The position in the PHP_CodeSniffer file's token stack where the token was found.
+	 *
+	 * @return void
+	 */
 	public function process( File $phpcsFile, $stackPtr ) {
 		$tokens = $phpcsFile->getTokens();
 
