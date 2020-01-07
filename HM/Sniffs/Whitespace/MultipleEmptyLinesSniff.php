0b344e522647bab2f8408ab1cf6c0731a4ef1178
Mike Selander <mikeselander@gmail.com>
2020-01-07 08:05:46 -0700

    Use early returns instead of nested ifs

--- HM/Sniffs/Whitespace/MultipleEmptyLinesSniff.php
+++ HM/Sniffs/Whitespace/MultipleEmptyLinesSniff.php
@@ -41,39 +41,53 @@ class MultipleEmptyLinesSniff implements Sniff {
 	public function process( File $phpcsFile, $stackPtr ) {
 		$tokens = $phpcsFile->getTokens();
 
-		if (
-			$stackPtr > 2
-			&& $tokens[ $stackPtr - 1 ]['line'] < $tokens[ $stackPtr ]['line']
-			&& $tokens[ $stackPtr - 2 ]['line'] === $tokens[ $stackPtr - 1 ]['line']
-		) {
-			// This is the first whitespace token on a line
-			// and the line before this one is not empty,
-			// so this could be the start of a multiple empty line block.
-			$next = $phpcsFile->findNext( T_WHITESPACE, $stackPtr, null, true );
-			$lines = ( $tokens[ $next ]['line'] - $tokens[ $stackPtr ]['line'] );
-
-			if ( $lines > 1 ) {
-				// If the next non T_WHITESPACE token is more than 1 line away,
-				// then there were multiple empty lines.
-				$error = 'Multiple empty lines should not exist in a row; found %s consecutive empty lines';
-				$fix   = $phpcsFile->addFixableError(
-					$error,
-					$stackPtr,
-					'MultipleEmptyLines',
-					[ $lines ]
-				);
-
-				if ( $fix === true ) {
-					$phpcsFile->fixer->beginChangeset();
-					$i = $stackPtr;
-					while ( $tokens[ $i ]['line'] !== $tokens[ $next ]['line'] ) {
-						$phpcsFile->fixer->replaceToken( $i, '' );
-						$i++;
-					}
-					$phpcsFile->fixer->addNewlineBefore( $i );
-					$phpcsFile->fixer->endChangeset();
-				}
-			}
+		// Only continue if the line position is greater than a file opener line.
+		if ( $stackPtr <= 2 ) {
+			return;
 		}
+
+		if ( $tokens[ $stackPtr - 1 ]['line'] >= $tokens[ $stackPtr ]['line'] ) {
+			return;
+		}
+
+		if ( $tokens[ $stackPtr - 2 ]['line'] !== $tokens[ $stackPtr - 1 ]['line'] ) {
+			return;
+		}
+
+		// This is the first whitespace token on a line
+		// and the line before this one is not empty,
+		// so this could be the start of a multiple empty line block.
+		$next = $phpcsFile->findNext( T_WHITESPACE, $stackPtr, null, true );
+		$lines = ( $tokens[ $next ]['line'] - $tokens[ $stackPtr ]['line'] );
+
+		// If there's only one whitespace line, this sniff does not apply.
+		if ( $lines <= 1 ) {
+			return;
+		}
+
+		// If the next non T_WHITESPACE token is more than 1 line away,
+		// then there were multiple empty lines.
+		$error = 'Multiple empty lines should not exist in a row; found %s consecutive empty lines';
+		$fix   = $phpcsFile->addFixableError(
+			$error,
+			$stackPtr,
+			'MultipleEmptyLines',
+			[ $lines ]
+		);
+
+		// Only continue if we're in fixing mode.
+		if ( $fix !== true ) {
+			return;
+		}
+
+		$phpcsFile->fixer->beginChangeset();
+		$i = $stackPtr;
+		while ( $tokens[ $i ]['line'] !== $tokens[ $next ]['line'] ) {
+			$phpcsFile->fixer->replaceToken( $i, '' );
+			$i++;
+		}
+
+		$phpcsFile->fixer->addNewlineBefore( $i );
+		$phpcsFile->fixer->endChangeset();
 	}
 }
