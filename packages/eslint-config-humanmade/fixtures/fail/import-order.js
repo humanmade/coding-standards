2856040e613c1e619d1c420ce88287b407b91ebd
tfrommen <info@tfrommen.de>
2020-08-19 15:48:55 +0200

    ESLint: Add and configure import plugin

---
+++ packages/eslint-config-humanmade/fixtures/fail/import-order.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+
+import apiFetch from '@wordpress/api-fetch';
+import chalk from 'chalk';
+import eslint from 'eslint';
+import path from 'path';
+import './';
+import Test from './component-jsx-parentheses';
+import './style.scss';
+import index from '../../index';
+import '../test-lint-config';
---
+++ packages/eslint-config-humanmade/fixtures/pass/import-order.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+import path from 'path';
+
+import chalk from 'chalk';
+import eslint from 'eslint';
+
+import apiFetch from '@wordpress/api-fetch';
+
+import index from '../../index';
+import '../test-lint-config';
+
+import Test from './component-jsx-parentheses';
+
+import './';
+import './style.scss';
